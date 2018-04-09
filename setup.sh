40e2ce8ca0a3b68a49d86b513191fb0a1b703244
0koara0 <i0koara0@gmail.com>
2018-04-09 21:51:14 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,6 @@ ln -sf ~/dotfiles/bundle ~/.vim
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/zsh-syntax-highlighting ~/
-
+chsh -s /bin/zsh
+git submodule init
+git submodule update
